781572e2d15a9d6eca2017bc1fcf2115d9b4ad13
samson <samseyi672@gmail.com>
2020-06-22 00:00:55 +0100

    project first commit

---
+++ src/main/java/com/mint/financial/MintFinancialSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.mint.financial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MintFinancialSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MintFinancialSolutionApplication.class, args);
+	}
+
+}
