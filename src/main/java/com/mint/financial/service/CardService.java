9a847916ecbff22f6e40a2180dfd8689e450e057
samson <samseyi672@gmail.com>
2020-06-22 00:27:22 +0100

    ssl configuration was created

--- src/main/java/com/mint/financial/service/CardService.java
+++ src/main/java/com/mint/financial/service/CardService.java
@@ -1,5 +1,25 @@
 package com.mint.financial.service;
 
-public class CardService {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.mint.financial.SSLContextHelper;
+import com.mint.financial.entity.CardScheme;
+
+@Service
+public class CardService {
+	
+	  @Autowired
+	 RestTemplate template  ;
+	 @Autowired
+	 SSLContextHelper ssl ;
+	public CardScheme findCardDetails(String url , String cardNumber) {
+		// create  the card  instance here 
+		  ssl.disable();
+		 String result = template.getForObject(url+"/"+cardNumber, String.class);	     
+		    System.out.println("from "+ result);
+		CardScheme card  =   template.getForObject(url+"/"+cardNumber,CardScheme.class) ;
+		   return card ;
+	}
 }
